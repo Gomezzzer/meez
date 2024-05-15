8bfc99544b1434b9a681fc11a1f7dfe5d6dace91
gomezzzer <gomezgeorge132@gmail.com>
2024-05-15 17:35:42 -0500

    Revert "Merge branch 'footer' into meez-en-place"
    This reverts commit 3b3a2097eb67d16125109f78550653cfcb5d4739, reversing
    changes made to 81bc32ee37f91430deef83fdd940bacd80f05b43.

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
     get :desired_yield_page, on: :collection
     post 'scale_recipe', on: :collection
   end
-
   devise_for :users
-
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # config/routes.rb
+ 
+ 
   
 end
