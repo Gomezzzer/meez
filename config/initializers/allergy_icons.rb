fc1f93521bdd951ff92b39680ffcef5361d42c40
gomezzzer <gomezgeorge132@gmail.com>
2024-05-28 14:59:06 -0500

    Added vegan/veg to icons, edit card

--- config/initializers/allergy_icons.rb
+++ config/initializers/allergy_icons.rb
@@ -1,5 +1,5 @@
 # config/initializers/allergy_icons.rb
 
-allergy_icons = %w(peanut.png dairy.png gluten.png lobster.png)
+allergy_icons = %w(peanut.png dairy.png gluten.png lobster.png vegan.png vegetarian.png)
 
 Rails.application.config.assets.precompile += allergy_icons
