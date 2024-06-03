3af92c0a1081da86aaf475c3e5a5595ef1f282e1
gomezzzer <gomezgeorge132@gmail.com>
2024-06-02 22:36:43 -0500

    Added chartkick

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
+import "chartkick"
+import "Chart.bundle"
 
 
 
@@ -9,6 +10,3 @@ import jQuery from "jquery";
 window.jQuery = window.$ = jQuery;
 
 import "rails-ujs"; // Import jQuery-ujs
-
-
-
